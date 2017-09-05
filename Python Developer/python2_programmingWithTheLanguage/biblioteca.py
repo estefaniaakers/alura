9fcc0e65bca00249221795aab822c7504336d177
estefaniaakers <estefania.silva2@fatec.sp.gov.br>
2017-09-04 21:06:19 -0300

    Using definition in python (def) - biblioteca

---
+++ Python Developer/python2_programmingWithTheLanguage/biblioteca.py
@@ -0,0 +1,15 @@
+# E:\Alura\Python Developer\python2_programmingWithTheLanguage\biblioteca.py
+
+def gera_nome_convite(nome_convidado):
+    posicao_final = len(nome_convidado)
+    posicao_inicial = posicao_final - 4
+    parte1 = nome_convidado[0:4]
+    parte2 = nome_convidado[posicao_inicial:posicao_final]
+    return (parte1 + ' ' + parte2)
+
+def envia_convite(nome_convidado):
+    print ("Enviando convite para %s" %nome_convidado)
+    
+def processa_convite(nome_convidado):
+    nome_formatado = gera_nome_convite(nome_convidado)
+    envia_convite(nome_formatado)
